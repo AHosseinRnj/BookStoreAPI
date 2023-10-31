0fc990b5b9d60024b176045f88aac8953cebdb9d
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-31 13:23:08 +0330

    Add Order Entity

---
+++ BookStoreWebAPI/Domain/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
