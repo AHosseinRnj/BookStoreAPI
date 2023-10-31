035f03e0a3070a6115518376546367e122cd2943
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-10-31 13:27:08 +0330

    Add Two more properties

--- BookStoreWebAPI/Domain/Entities/OrderBook.cs
+++ BookStoreWebAPI/Domain/Entities/OrderBook.cs
@@ -4,6 +4,7 @@
     {
         public int OrderId { get; set; }
         public int BookId{ get; set; }
-    }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
     }
 }
