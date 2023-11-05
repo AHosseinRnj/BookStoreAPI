300c9efa980035c528b978e9a4cde5577d11f2b2
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-05 10:36:20 +0330

    Move OrderBook logging from presentation to application layer

--- BookStoreWebAPI/Application/Commands/OrderBook/CreateOrderBook/CreateOrderBookHandler.cs
+++ BookStoreWebAPI/Application/Commands/OrderBook/CreateOrderBook/CreateOrderBookHandler.cs
@@ -1,21 +1,37 @@
 ﻿using Application.Repositpries;
+using log4net;
 using MediatR;
 
 namespace Application.Commands.CreateOrderBook
 {
     public class CreateOrderBookHandler : IRequestHandler<CreateOrderBookCommand, Unit>
     {
+        private readonly ILog _logger;
         private IUnitOfWork _unitOfWork;
         public CreateOrderBookHandler(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+            _logger = LogManager.GetLogger(typeof(CreateOrderBookHandler));
         }
 
         public async Task<Unit> Handle(CreateOrderBookCommand request, CancellationToken cancellationToken)
         {
-            await _unitOfWork.OrderBookRepository.AddAsync(request);
-            _unitOfWork.Commit();
+            try
+            {
+                _logger.Info("Received a request to add an OrderBook.");
+                await _unitOfWork.OrderBookRepository.AddAsync(request);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error adding an OrderBook: " + ex.Message, ex);
+                throw;
+            }
+            finally
+            {
+                _unitOfWork.Commit();
+            }
 
+            _logger.Info("OrderBook added successfully.");
             return Unit.Value;
         }
     }
--- BookStoreWebAPI/Application/Query/OrderBook/GetOrderBooks/GetOrderBooksQueryHandler.cs
+++ BookStoreWebAPI/Application/Query/OrderBook/GetOrderBooks/GetOrderBooksQueryHandler.cs
@@ -1,22 +1,41 @@
 ﻿using Application.Query.GetOrderBook;
 using Application.Repositpries;
+using log4net;
 using MediatR;
+using System.Collections.Generic;
 
 namespace Application.Query.GetOrderBooks
 {
     public class GetOrderBooksQueryHandler : IRequestHandler<GetOrderBooksQuery, IEnumerable<GetOrderBookQueryResponse>>
     {
+        private readonly ILog _logger;
         private IUnitOfWork _unitOfWork;
         public GetOrderBooksQueryHandler(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+            _logger = LogManager.GetLogger(typeof(GetOrderBooksQueryHandler));
         }
 
         public async Task<IEnumerable<GetOrderBookQueryResponse>> Handle(GetOrderBooksQuery request, CancellationToken cancellationToken)
         {
-            var orderBooks = await _unitOfWork.OrderBookRepository.GetOrderBooksAsync();
-            _unitOfWork.Commit();
+            IEnumerable<GetOrderBookQueryResponse> orderBooks;
 
+            try
+            {
+                _logger.Info("Received a request to get all OrderBooks");
+                orderBooks = await _unitOfWork.OrderBookRepository.GetOrderBooksAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Error getting OrderBooks: " + ex.Message, ex);
+                throw;
+            }
+            finally
+            {
+                _unitOfWork.Commit();
+            }
+
+            _logger.Info("OrderBooks retrieved successfully.");
             return orderBooks;
         }
     }
