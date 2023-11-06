f78646a5e3819221dc20db4b52704d0b6eefa145
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-06 15:12:32 +0330

    Fix AuthorRepository

--- BookStoreWebAPI/Infrastructure/Persistance/Repositories/AuthorRepository.cs
+++ BookStoreWebAPI/Infrastructure/Persistance/Repositories/AuthorRepository.cs
@@ -1,9 +1,6 @@
-﻿using Application.Commands.CreateAuthor;
-using Application.Commands.UpdateAuthor;
-using Application.Query.Author.GetAuthor;
-using Application.Query.GetBook;
-using Application.Repositpries;
+﻿using Application.Repositpries;
 using Dapper;
+using Domain.Entities;
 using System.Data;
 
 namespace Infrastructure.Persistance.Repositories
@@ -16,7 +13,7 @@ namespace Infrastructure.Persistance.Repositories
             _dapperContext = dapperContext;
         }
 
-        public async Task AddAsync(CreateAuthorCommand author)
+        public async Task AddAsync(Author author)
         {
             var query = "INSERT INTO Author (Id, FirstName, LastName, Description) VALUES (@Id, @FirstName, @LastName, @Description)";
 
@@ -35,35 +32,35 @@ namespace Infrastructure.Persistance.Repositories
             await _dapperContext.Connection.ExecuteAsync(query, new { id }, _dapperContext.Transaction);
         }
 
-        public async Task<IEnumerable<GetAuthorQueryResponse>> GetAuthorsAsync()
+        public async Task<IEnumerable<Author>> GetAuthorsAsync()
         {
             var query = "SELECT * FROM Author";
-            var listOfAuthors = await _dapperContext.Connection.QueryAsync<GetAuthorQueryResponse>(query, null, _dapperContext.Transaction);
+            var listOfAuthors = await _dapperContext.Connection.QueryAsync<Author>(query, null, _dapperContext.Transaction);
 
             return listOfAuthors;
         }
 
-        public async Task<GetAuthorQueryResponse> GetAuthorById(int id)
+        public async Task<Author> GetAuthorById(int id)
         {
             var query = "SELECT * FROM Author WHERE id = @Id";
-            var author = await _dapperContext.Connection.QueryFirstAsync<GetAuthorQueryResponse>(query, new { id }, _dapperContext.Transaction);
+            var author = await _dapperContext.Connection.QueryFirstAsync<Author>(query, new { id }, _dapperContext.Transaction);
 
             return author;
         }
-        public async Task<IEnumerable<GetBookQueryResponse>> GetAuthorBooksAsync(int id)
+        public async Task<IEnumerable<Book>> GetAuthorBooksAsync(int id)
         {
             var query = "SELECT Book.Title, Book.ISBN, Book.Price FROM Book JOIN Author ON Book.AuthorId = Author.Id WHERE Author.Id = @AutId";
-            var listOfBooks = await _dapperContext.Connection.QueryAsync<GetBookQueryResponse>(query, new { AutId = id }, _dapperContext.Transaction);
+            var listOfBooks = await _dapperContext.Connection.QueryAsync<Book>(query, new { AutId = id }, _dapperContext.Transaction);
 
             return listOfBooks;
         }
 
-        public async Task UpdateAsync(UpdateAuthorCommand author)
+        public async Task UpdateAsync(Author author)
         {
             var query = "UPDATE Author SET firstname = @FirstName, lastname = @LastName, description = @Description WHERE Id=@Id";
 
             var parameters = new DynamicParameters();
-            parameters.Add("id", author.id, DbType.Int32);
+            parameters.Add("id", author.Id, DbType.Int32);
             parameters.Add("FirstName", author.FirstName, DbType.String);
             parameters.Add("LastName", author.LastName, DbType.String);
             parameters.Add("Description", author.Description, DbType.String);
