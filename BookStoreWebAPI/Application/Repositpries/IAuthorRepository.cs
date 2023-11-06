6ff6203f84e0e28b3125fe5014636b9567a05fdb
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-06 14:53:31 +0330

    Change IAuthorRepository Parameters

--- BookStoreWebAPI/Application/Repositpries/IAuthorRepository.cs
+++ BookStoreWebAPI/Application/Repositpries/IAuthorRepository.cs
@@ -1,17 +1,14 @@
-﻿using Application.Commands.CreateAuthor;
-using Application.Commands.UpdateAuthor;
-using Application.Query.Author.GetAuthor;
-using Application.Query.GetBook;
+﻿using Domain.Entities;
 
 namespace Application.Repositpries
 {
     public interface IAuthorRepository
     {
-        Task AddAsync(CreateAuthorCommand author);
-        Task<IEnumerable<GetAuthorQueryResponse>> GetAuthorsAsync();
-        Task<GetAuthorQueryResponse> GetAuthorById(int id);
-        Task<IEnumerable<GetBookQueryResponse>> GetAuthorBooksAsync(int id);
-        Task UpdateAsync(UpdateAuthorCommand author);
+        Task AddAsync(Author author);
+        Task<IEnumerable<Author>> GetAuthorsAsync();
+        Task<Author> GetAuthorById(int id);
+        Task<IEnumerable<Book>> GetAuthorBooksAsync(int id);
+        Task UpdateAsync(Author author);
         Task DeleteByIdAsync(int id);
     }
 }
