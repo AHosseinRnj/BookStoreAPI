e7ef28c0cc8dc0af16f952f23040a94facc4ec23
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-06 14:44:33 +0330

    Add IAuthorService

---
+++ BookStoreWebAPI/Application/Services/IAuthorService.cs
@@ -0,0 +1,17 @@
+﻿using Application.Commands.CreateAuthor;
+using Application.Commands.UpdateAuthor;
+using Application.Query.Author.GetAuthor;
+using Application.Query.GetBook;
+
+namespace Application.Services
+{
+    public interface IAuthorService
+    {
+        Task AddAsync(CreateAuthorCommand author);
+        Task<IEnumerable<GetAuthorQueryResponse>> GetAuthorsAsync();
+        Task<GetAuthorQueryResponse> GetAuthorById(int id);
+        Task<IEnumerable<GetBookQueryResponse>> GetAuthorBooksAsync(int id);
+        Task UpdateAsync(UpdateAuthorCommand author);
+        Task DeleteByIdAsync(int id);
+    }
+}
