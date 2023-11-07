3271fa44b78950a44bf285cca01c7c5fd5bd66ba
Amirhossein Ranjbaran <amirhosein.ranjbaran1380@gmail.com>
2023-11-07 13:13:13 +0330

    Remove TotalPrice from Order class

--- BookStoreWebAPI/Domain/Entities/Order.cs
+++ BookStoreWebAPI/Domain/Entities/Order.cs
@@ -3,7 +3,6 @@
     public class Order
     {
         public int Id { get; set; }
-        public double TotalPrice { get; set; }
         public DateTime OrderDate { get; set; }
 
         public int UserId { get; set; }
